6d7233765b249274fc07ca0b612d0880f1abd4e0
Thiahho <110945977+Thiahho@users.noreply.github.com>
2025-09-21 12:54:40 -0300

    2109

--- frontend/src/assets/assets/assets.js
+++ frontend/src/assets/assets/assets.js
@@ -1,42 +1,22 @@
-import logo from "./logo.svg";
-import logo_dark from "./logo_dark.svg";
-import cross_icon from "./cross_icon.svg";
-import menu_icon from "./menu_icon.svg";
-import star_icon from "./star_icon.svg";
-import left_arrow from "./left_arrow.svg";
-import right_arrow from "./right_arrow.svg";
-// import right_arrow from './right_arrow.svg'
-import header_img from "./header_img.png";
-import brand_img from "./brand_img.png";
-import project_img_1 from "./project_img_1.jpg";
-import project_img_2 from "./project_img_2.jpg";
-import project_img_3 from "./project_img_3.jpg";
-import project_img_4 from "./project_img_4.jpg";
-import project_img_5 from "./project_img_5.jpg";
-import project_img_6 from "./project_img_6.jpg";
-import profile_img_1 from "./profile_img_1.png";
-import profile_img_2 from "./profile_img_2.png";
-import profile_img_3 from "./profile_img_3.png";
-
 export const assets = {
-  logo,
-  logo_dark,
-  cross_icon,
-  menu_icon,
-  star_icon,
-  header_img,
-  brand_img,
-  project_img_1,
-  project_img_2,
-  project_img_3,
-  project_img_4,
-  project_img_5,
-  project_img_6,
-  left_arrow,
-  right_arrow,
-  profile_img_1,
-  profile_img_2,
-  profile_img_3,
+  logo: "/logo.svg",
+  logo_dark: "/logo_dark.svg",
+  cross_icon: "/cross_icon.svg",
+  menu_icon: "/menu_icon.svg",
+  star_icon: "/star_icon.svg",
+  header_img: "/header_img.png",
+  brand_img: "/brand_img.png",
+  project_img_1: "/project_img_1.jpg",
+  project_img_2: "/project_img_2.jpg",
+  project_img_3: "/project_img_3.jpg",
+  project_img_4: "/project_img_4.jpg",
+  project_img_5: "/project_img_5.jpg",
+  project_img_6: "/project_img_6.jpg",
+  left_arrow: "/left_arrow.svg",
+  right_arrow: "/right_arrow.svg",
+  profile_img_1: "/profile_img_1.png",
+  profile_img_2: "/profile_img_2.png",
+  profile_img_3: "/profile_img_3.png",
 };
 
 export const projectsData = [
@@ -44,37 +24,37 @@ export const projectsData = [
     title: "Skyline Haven",
     price: "$2,50,000",
     location: "California",
-    image: project_img_1,
+    image: assets.project_img_1,
   },
   {
     title: "Vista Verde",
     price: "$2,50,000",
     location: "San Francisco",
-    image: project_img_2,
+    image: assets.project_img_2,
   },
   {
     title: "Serenity Suites",
     price: "$2,50,000",
     location: "Chicago",
-    image: project_img_3,
+    image: assets.project_img_3,
   },
   {
     title: "Central Square",
     price: "$2,50,000",
     location: "Los Angeles",
-    image: project_img_4,
+    image: assets.project_img_4,
   },
   {
     title: "Vista Verde",
     price: "$2,50,000",
     location: "San Francisco",
-    image: project_img_5,
+    image: assets.project_img_5,
   },
   {
     title: "Serenity Suites",
     price: "$2,50,000",
     location: "Chicago",
-    image: project_img_6,
+    image: assets.project_img_6,
   },
 ];
 
@@ -82,7 +62,7 @@ export const testimonialsData = [
   {
     name: "Donald Jackman",
     title: "Marketing Manager",
-    image: profile_img_1,
+    image: assets.profile_img_1,
     alt: "Portrait of Donald Jackman",
     rating: 5,
     text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched.",
@@ -90,7 +70,7 @@ export const testimonialsData = [
   {
     name: "Richard Nelson",
     title: "UI/UX Designer",
-    image: profile_img_2,
+    image: assets.profile_img_2,
     alt: "Portrait of Richard Nelson",
     rating: 4,
     text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched.",
@@ -98,7 +78,7 @@ export const testimonialsData = [
   {
     name: "James Washington",
     title: "Co-Founder",
-    image: profile_img_3,
+    image: assets.profile_img_3,
     alt: "Portrait of James Washington",
     rating: 5,
     text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched.",
